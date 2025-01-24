303225a7dfbb5f68cdf73494ff45b74f293fbc73
Azeem Shaik <azeemshaik025@gmail.com>
2025-01-24 21:48:42 +0530

    fix: show proper logs

--- src/components/poll/manage/ManagePollParent.tsx
+++ src/components/poll/manage/ManagePollParent.tsx
@@ -10,11 +10,12 @@ export default function ManagePollParent({ polls }: {
 }) {
 
     const username = useUserStore((state) => state.username);
+    const userPolls = polls?.filter((poll) => poll.username === username);
 
     return (
         <div className="flex mt-20 mx-8 flex-wrap items-center justify-evenly px-5 gap-x-10 gap-y-10">
-            {Array.isArray(polls) && polls.length > 0 ? (
-                polls?.filter((poll) => poll.username === username)?.map((poll, index) => (
+            {Array.isArray(userPolls) && userPolls.length > 0 ? (
+                userPolls.map((poll, index) => (
                     <div
                         className="hover:scale-105 hover:rotate-2 transition-all duration-75 bg-white shadow-[0_4px_6px_-1px_rgba(0,0,0,0.1),0_2px_4px_-1px_rgba(0,0,0,0.06)] hover:shadow-[0_10px_15px_-3px_rgba(0,0,0,0.1),0_4px_6px_-2px_rgba(0,0,0,0.05)] flex flex-col gap-[10px] w-[15rem] h-[16rem] justify-center items-start rounded-xl px-2"
                         key={poll.pollId}
@@ -34,7 +35,10 @@ export default function ManagePollParent({ polls }: {
                     </div>
                 ))
             ) : (
-                <p className="text-gray-600">There are no polls currently.</p>
+                <div className="text-center p-8 rounded-lg bg-gray-50 shadow-sm">
+                    <h3 className="text-xl font-semibold text-gray-800 mb-2">No Polls Found</h3>
+                    <p className="text-gray-600">You haven't created any polls yet. Create a new poll to get started!</p>
+                </div>
             )}
         </div>
     );
